0b3c9223e8a78f6c2d822ebf205ffa88d49160c5
Jennifer <jenleebell@gmail.com>
2015-08-28 10:13:06 -0700

    Pass spec for bands method in venue class

--- spec/venue_spec.rb
+++ spec/venue_spec.rb
@@ -10,4 +10,13 @@ describe(Venue) do
     test_venue = Venue.create({:name => "moda center"})
     expect(test_venue.name()).to(eq("Moda Center"))
   end
+
+  describe('#bands') do
+    it('lists all of bands the venue has hosted') do
+      test_band = Band.create({:name => "pearl jam"})
+      test_venue = Venue.create({:name => "moda center"})
+      test_venue.bands.push(test_band)
+      expect(test_venue.bands()).to(eq([test_band]))
+    end
+  end
 end
