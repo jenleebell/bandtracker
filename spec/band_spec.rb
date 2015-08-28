7e8bfaa3193a194f75e42ef6f982d6bff91a56d5
Jennifer <jenleebell@gmail.com>
2015-08-28 09:52:09 -0700

    Pass spec to capitalize name of band

--- spec/band_spec.rb
+++ spec/band_spec.rb
@@ -5,4 +5,9 @@ describe(Band) do
     test_band = Band.new({:name => ""})
     expect(test_band.save()).to(eq(false))
   end
+
+  it('capitalizes the first letter of each word in the name') do
+    test_band = Band.create({:name => "pearl jam"})
+    expect(test_band.name()).to(eq("Pearl Jam"))
+  end
 end
