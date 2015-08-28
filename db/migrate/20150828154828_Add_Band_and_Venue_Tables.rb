0cc173e39a253af957c10da214823caafff8611d
Jennifer <jenleebell@gmail.com>
2015-08-28 08:58:28 -0700

    Add tables for all classes

---
+++ db/migrate/20150828154828_Add_Band_and_Venue_Tables.rb
@@ -0,0 +1,20 @@
+class AddBandAndVenueTables < ActiveRecord::Migration
+  def change
+    create_table(:bands) do |t|
+      t.column(:name, :string)
+
+      t.timestamps
+    end
+
+    create_table(:venues) do |t|
+      t.column(:name, :string)
+
+      t.timestamps
+    end
+
+    create_table(:bands_venues) do |t|
+      t.column(:band_id, :integer)
+      t.column(:venue_id, :integer)
+    end
+  end
+end
